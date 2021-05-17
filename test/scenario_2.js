5c735ff1ecf640d8ce30b848df7173eb89b6b592
cjaurechi <cjaurechi@gmail.com>
2021-05-16 23:45:36 -0300

    Updated scenario 2 test

--- test/scenario_2.js
+++ test/scenario_2.js
@@ -12,16 +12,13 @@ const { assert } = require("../utils/assertions");
   const page = await context.newPage();
 
   const timelinePage = new TimelinePage(page);
-  // Navigates to Mercari website
   await timelinePage.navigate(userData.baseUrl);
-  // Searches for an specific item
   await timelinePage.searchItem(userData.item);
   
   const searchResultPage = new SearchResultPage(page);
-  // Gets the title of the item in the specified position
+
   const itemTitle = await searchResultPage.getItemTitle(3);
 
-  // Validates the searched item name is included on the item title
   await assert(itemTitle, userData.item);
 
   await context.close();
