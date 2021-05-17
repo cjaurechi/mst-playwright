019ed2d50866e3bd50dbaf87dce284055f1e1f59
cjaurechi <cjaurechi@gmail.com>
2021-05-16 23:45:23 -0300

    Updated scenario 1 test

--- test/scenario_1.js
+++ test/scenario_1.js
@@ -8,6 +8,10 @@ const { ShippingAddressPage } = require("../pageObject/ShippingAddressPage");
 const { verifyAddress } = require("../utils/assertions");
 
 (async () => {
+  /**
+   * For this scenario I'm assuming the user doesn't have any previous shipping address
+   * saved on his profile as we're testing the "add new shipping address" happy path.
+   */
   const browser = await chromium.launch({
     headless: false,
   });
@@ -17,7 +21,7 @@ const { verifyAddress } = require("../utils/assertions");
   const timelinePage = new TimelinePage(page);
   await timelinePage.navigate(userData.baseUrl);
   await timelinePage.clickLogin();
-  
+
   const loginPage = new LoginPage(page);
   await loginPage.login(userData.username, userData.password);
 
@@ -29,7 +33,21 @@ const { verifyAddress } = require("../utils/assertions");
 
   const shippingAddressPage = new ShippingAddressPage(page);
   await shippingAddressPage.createNewShippingAddress(shippingAddress);
-  await verifyAddress(shippingAddress);
+
+  /** 
+   * I'm assuming there is only one address (The recently created one) and that's
+   * why I'm using page.$('.address-box') assuming it's the only element there. I'm
+   * also assuming address-box is the class of the element containing the address.
+   */
+  const newShippingAddress = page.$(".address-box");
+  await verifyAddress(newShippingAddress, shippingAddress);
+
+  /** 
+   * Ideally we delete the newly created address at the end of the test. If we were using
+   * jest-playwright or Cypress we could use an "after" hook to do this and closing both
+   * browser and context instances.
+   */
+  await shippingAddressPage.deleteShippingAddress(shippingAddress);
 
   await context.close();
   await browser.close();
