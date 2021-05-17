88956757ab09f8069c07c2fce3187f6d8dff5b76
cjaurechi <cjaurechi@gmail.com>
2021-05-16 23:46:27 -0300

    Added deleteShippingAddress method

--- pageObject/ShippingAddressPage.js
+++ pageObject/ShippingAddressPage.js
@@ -6,7 +6,8 @@ class ShippingAddressPage extends MyPagePage {
   }
 
   async createNewShippingAddress({ data }) {
-    /** As I don't have access to the web app, I'm using the provided screenshots
+    /**
+     * As I don't have access to the web app, I'm using the provided screenshots
      * as a reference to infer the web elements selectors. Ideally I'd use representative
      * selectors that won't change soon, such as placeholder or text. To be even more
      * specific, I'd start by checking the web element (Input, button, div) and then
@@ -28,6 +29,24 @@ class ShippingAddressPage extends MyPagePage {
     await this.page.fill('input[placeholder="都道府県"]', data.prefecture);
     await this.page.fill('input[placeholder="市区町村"]', data.municipality);
     await this.page.click('button >> text="登録する"');
+  }
+
+  async deleteShippingAddress({ data }) {
+    /**
+     * I'm not familiar with Mercari's Shipping Address page and as the test only
+     * provides screenshots for the mobile app I'll assume there is a delete
+     * button right next to the element that contains the address.
+     */
+
+    const address = await this.page.$(
+      `button:right-of(:text("${data.address}")`
+    );
+    await address.click();
+    /**
+     * Once again I'm assuming there is a confirmation message showing up on the UI
+     * and the user must manually click on '削除する' to confirm the address to be deleted.
+     */
+    await this.page.click("text=削除する");
   }
 }
 
